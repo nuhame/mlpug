ac81419480a26df13c65ed41f63a696bb7a9a599
Freddy Snijder <freddy@visionscapers.com>
2020-04-19 12:44:59 +0200

    Expose the MetricsLoggingMode

--- mlpug/pytorch/trainers/callbacks/__init__.py
+++ mlpug/pytorch/trainers/callbacks/__init__.py
@@ -2,7 +2,7 @@ from .basic import *
 from .callback import *
 from .checkpoint_manager import CheckpointManager
 from .lr_scheduler_wrapper import LRSchedulerWrapper
-from .metrics_logger import TrainingMetricsLogger, TestMetricsLogger
+from .metrics_logger import MetricsLoggingMode, TrainingMetricsLogger, TestMetricsLogger
 from .tensorboard import Tensorboard, AutoTensorboard
 from .distributed import DistributedSamplerManager
 
