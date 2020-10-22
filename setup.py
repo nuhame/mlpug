895359d2f8cf5216948e2a1b6a2217d433d0db5b
Freddy Snijder <freddy@visionscapers.com>
2020-10-22 23:51:28 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mlpug",
-    version="0.0.16",
+    version="0.0.17",
     author="Freddy Snijder",
     author_email="mlpug@visionscapers.com",
     description="A machine learning library agnostic framework for model training",
