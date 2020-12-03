08452783d69c2325363594770a346010fed85088
Freddy Snijder <freddy@visionscapers.com>
2020-12-03 12:14:04 +0100

    Refactored out evaluation code from training script

--- mlpug/examples/chatbot/tensorflow/original_transformer_tutorial/chatbot_reference.py
+++ mlpug/examples/chatbot/tensorflow/original_transformer_tutorial/chatbot_reference.py
@@ -6,8 +6,6 @@ import sys
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-import matplotlib.pyplot as plt
-
 from mlpug.examples.chatbot.conversation_dataset import load_sentence_pair_data
 
 from mlpug.examples.chatbot.tensorflow.original_transformer_tutorial.model_data_generation import \
@@ -24,14 +22,14 @@ from mlpug.examples.chatbot.tensorflow.original_transformer_tutorial.transformer
 # ############# SETUP ###############
 REMOTE_DEBUG = False
 
-experiment_name = "large-transformer-30112020-lr-1e_4"
+experiment_name = "large-transformer-01122020-4-layers"
 
 data_set_path = "./mlpug/examples/chatbot/data/"
 
 training_set_file = "training-1606433382-cmdc-sentence-pairs-with-voc-max-len-40-min-word-occurance-3-26112020.pickle"
 validation_set_file = "validation-1606433382-cmdc-sentence-pairs-with-voc-max-len-40-min-word-occurance-3-26112020.pickle"
 
-num_layers = 12
+num_layers = 4
 d_model = 768
 dff = 3072
 num_heads = 12
@@ -102,9 +100,9 @@ val_dataset = val_dataset.filter(filter_max_length).padded_batch(BATCH_SIZE)
 
 
 # ######## SETUP OPTIMIZER ##########
-# learning_rate = CustomSchedule(d_model)
+learning_rate = CustomSchedule(d_model)
 
-learning_rate = 1e-4
+#learning_rate = 5e-3
 optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
@@ -154,94 +152,6 @@ def train_step(inp, tar):
 
     train_loss(loss)
     train_accuracy(tar_real, predictions)
-
-
-def evaluate(inp_sentence):
-    start_token = [tokenizer.vocab_size]
-    end_token = [tokenizer.vocab_size + 1]
-
-    # inp sentence is portuguese, hence adding the start and end token
-    inp_sentence = start_token + tokenizer.encode(inp_sentence) + end_token
-    encoder_input = tf.expand_dims(inp_sentence, 0)
-
-    # as the target is english, the first word to the transformer should be the
-    # english start token.
-    decoder_input = [tokenizer.vocab_size]
-    output = tf.expand_dims(decoder_input, 0)
-
-    for i in range(MAX_LENGTH):
-        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
-            encoder_input, output)
-
-        # predictions.shape == (batch_size, seq_len, vocab_size)
-        predictions, attention_weights = transformer(encoder_input,
-                                                     output,
-                                                     False,
-                                                     enc_padding_mask,
-                                                     combined_mask,
-                                                     dec_padding_mask)
-
-        # select the last word from the seq_len dimension
-        predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
-
-        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-        # return the result if the predicted_id is equal to the end token
-        if predicted_id == tokenizer.vocab_size + 1:
-            return tf.squeeze(output, axis=0), attention_weights
-
-        # concatentate the predicted_id to the output which is given to the decoder
-        # as its input.
-        output = tf.concat([output, predicted_id], axis=-1)
-
-    return tf.squeeze(output, axis=0), attention_weights
-
-
-def plot_attention_weights(attention, sentence, result, layer):
-    fig = plt.figure(figsize=(16, 8))
-
-    sentence = tokenizer.encode(sentence)
-
-    attention = tf.squeeze(attention[layer], axis=0)
-
-    for head in range(attention.shape[0]):
-        ax = fig.add_subplot(2, 4, head + 1)
-
-        # plot the attention weights
-        ax.matshow(attention[head][:-1, :], cmap='viridis')
-
-        fontdict = {'fontsize': 10}
-
-        ax.set_xticks(range(len(sentence) + 2))
-        ax.set_yticks(range(len(result)))
-
-        ax.set_ylim(len(result) - 1.5, -0.5)
-
-        ax.set_xticklabels(
-            ['<start>'] + [tokenizer.decode([i]) for i in sentence] + ['<end>'],
-            fontdict=fontdict, rotation=90)
-
-        ax.set_yticklabels([tokenizer.decode([i]) for i in result
-                            if i < tokenizer.vocab_size],
-                           fontdict=fontdict)
-
-        ax.set_xlabel('Head {}'.format(head + 1))
-
-    plt.tight_layout()
-    plt.show()
-
-
-def translate(sentence, plot=''):
-    result, attention_weights = evaluate(sentence)
-
-    predicted_sentence = tokenizer.decode([i for i in result
-                                           if i < tokenizer.vocab_size])
-
-    print('Input: {}'.format(sentence))
-    print('Predicted translation: {}'.format(predicted_sentence))
-
-    if plot:
-        plot_attention_weights(attention_weights, sentence, result, plot)
 
 
 ckpt = tf.train.Checkpoint(transformer=transformer,
