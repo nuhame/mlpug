f8bd030900a22c822dcb2432adfd4d27910482a4
Freddy Snijder <freddy@visionscapers.com>
2021-07-10 00:35:21 +0200

    Added arguments related to checkpoint management

--- mlpug/examples/documentation/shared_args.py
+++ mlpug/examples/documentation/shared_args.py
@@ -5,22 +5,32 @@ def base_argument_set(description='Train model'):
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument(
-        '--batch-size',
+        '--experiment-name',
+        type=str, required=False, default='training-test',
+        help='Experiment name')
+
+    parser.add_argument(
+        '--hidden_size',
+        type=int, required=False, default=128,
+        help='Model hidden size')
+
+    parser.add_argument(
+        '--batch_size',
         type=int, required=False, default=64,
         help='Batch size (per process/replica)')
 
     parser.add_argument(
-        '--learning-rate',
+        '--learning_rate',
         type=float, required=False, default=1e-3,
         help='Learning rate')
 
     parser.add_argument(
-        '--num-epochs',
+        '--num_epochs',
         type=int, required=False, default=10,
         help='Number of epochs to train')
 
     parser.add_argument(
-        '--progress-log-period',
+        '--progress_log_period',
         type=int, required=False, default=200,
         help='Period in global (batch) iterations before we log the training progress again.')
 
