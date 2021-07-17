f8aadb36b80b90f02b05a82dfa5876ed2bb17be0
Freddy Snijder <freddy@visionscapers.com>
2021-07-17 14:47:07 +0200

    Added TODO for future refactor

--- mlpug/pytorch/multi_processing.py
+++ mlpug/pytorch/multi_processing.py
@@ -5,6 +5,12 @@ from mlpug.multi_processing import \
     MultiProcessingMixin
 
 
+# TODO : All the mlpug components that derive from MultiProcessingMixin, here in the pytorch package
+#        should be in a seperate package. This creates an intermediate level between the mlpug base classes
+#        and the Pytorch specializations. In this way the MultiProcessing versions of the components can easily be
+#        reused for another specialization.
+
+
 class PyTorchDistributedContext(MultiProcessingContextBase):
 
     def __init__(self, name="PyTorchDistributedContext"):
