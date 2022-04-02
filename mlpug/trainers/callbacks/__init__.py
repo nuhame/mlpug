1ca80fb0b9b3533d411e0723d6905a8f63c94ef7
Freddy Snijder <freddy@visionscapers.com>
2022-04-02 02:20:17 +0200

    Import abstract callback classes just like with the implementations

---
+++ mlpug/trainers/callbacks/__init__.py
@@ -0,0 +1,6 @@
+from .basic import *
+from .callback import *
+from .checkpoint_manager import CheckpointManager
+from .lr_scheduler_wrapper import LRSchedulerWrapper
+from .metrics_logger import MetricsLoggingMode, TrainingMetricsLogger, TestMetricsLogger
+from .tensorboard import Tensorboard, AutoTensorboard
