5a9ba67c84f6b1cff8add7fdc13ce8bcfb02d8bf
Freddy Snijder <freddy@visionscapers.com>
2022-09-17 14:40:32 +0200

    WIP: first version of Tensorflow specific training script for Chatbot training

---
+++ examples/chatbot/tensorflow/train.py
@@ -0,0 +1,294 @@
+import os
+
+from basics.logging_utils import log_exception
+from basics.logging import get_logger
+
+import mlpug.tensorflow as mlp
+
+from examples.chatbot.training_process import TrainingProcess as TrainingProcessBase
+from examples.chatbot.tensorflow.collation import MultipleChoiceCollator, CollatedSampleGenerator
+
+# TODO: Is activation --activation-checkpointing available for Huggingface TF models?
+from examples.chatbot.shared_args import create_arg_parser, describe_args
+
+
+module_logger = get_logger(os.path.basename(__file__))
+
+
+try:
+    import tensorflow as tf
+except Exception as e:
+    log_exception(module_logger, "Please `pip install tensorflow`", e)
+
+
+try:
+    from transformers import TFGPT2DoubleHeadsModel, AdamWeightDecay
+except Exception as e:
+    log_exception(module_logger, "Please `pip install transformers`", e)
+
+
+def gather_next_sentence_prediction_data(batch, auxiliary_results, **kwargs):
+    # Batch is a tuple with the following items :
+    # [0] input_ids_batch,
+    # [1] token_type_ids_batch,
+    # [2] token_labels_ids_batch,
+    # [3] last_token_idx_batch,
+    # [4] reply_class_batch
+
+    targets = batch[4]
+
+    nsp_logits = auxiliary_results["nsp_logits"]
+
+    prediction_probability = tf.nn.softmax(nsp_logits, axis=1)
+    predictions = tf.math.argmax(prediction_probability, axis=1)
+
+    return targets, predictions
+
+
+def clean_up_batch_data(model_output, **kwargs):
+    loss = model_output["loss"]
+
+    # Handy for debugging purposes to keep the scalar value
+    model_output["loss"] = loss.numpy()
+
+    # We don't need the auxiliary_results anymore
+    del model_output["auxiliary_results"]
+
+
+# MLPug needs a TrainModel that outputs the loss
+class TrainModel(tf.keras.Model):
+    def __init__(self, model, lm_loss_weight, global_batch_size):
+        super(TrainModel, self).__init__()
+
+        self.model = model
+        self.lm_loss_weight = lm_loss_weight
+        self.global_batch_size = global_batch_size
+
+        self.lm_loss_func = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.SUM)
+
+        self.mc_loss_func = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.SUM)
+
+    def call(self, batch_data, evaluate_settings, inference_mode=None):
+        input_ids_batch, \
+            token_type_ids_batch, \
+            token_labels_ids_batch, \
+            last_token_idx_batch, \
+            reply_class_batch = batch_data
+
+        results = self.model(
+            input_ids=input_ids_batch,
+            token_type_ids=token_type_ids_batch,
+            mc_token_ids=last_token_idx_batch,
+            training=inference_mode is False)
+
+        shift_logits = results.logits[..., :-1, :]
+        shift_labels = token_labels_ids_batch[..., 1:]
+
+        mc_logits = results.mc_logits
+
+        lm_loss = self.lm_loss_func(shift_labels, shift_logits) / self.global_batch_size
+        mc_loss = self.lm_loss_func(reply_class_batch, mc_logits) / self.global_batch_size
+
+        loss = (lm_loss*self.lm_loss_weight + mc_loss)/(self.lm_loss_weight+1.0)
+
+        return {
+            "loss": loss,
+            "num_samples": input_ids_batch.shape[0],
+            "auxiliary_results": {
+                # required to calculate next sentence prediction (classification) quality
+                "nsp_logits": mc_logits
+            }
+        }
+
+
+# See TrainingProcessBase for more information on the different methods implemented here
+# Here we implement the methods that are specific to our problem and specific to our ML library, Tensorflow.
+class TrainingProcess(TrainingProcessBase):
+
+    MLPUG_MODULE = mlp
+
+    def __init__(self, args, num_devices, name="TFTrainingProcess"):
+        """
+
+        :param args:
+        :param num_devices:
+        :param name:
+        """
+        # For Tensorflow we don't need a device rank, since the distribution works differently
+        super().__init__(0, args, num_devices, name=name)
+
+        self._distribution_strategy = None
+
+        self._global_batch_size = self.num_devices * self._args.batch_size
+
+    def _setup_compute(self):
+        if self.is_distributed:
+            devices = [f"/gpu:{i}" for i in range(self.num_devices)]
+            self._distribution_strategy = tf.distribute.MirroredStrategy(devices=devices)
+        else:
+            num_gpus_available = len(tf.config.list_physical_devices('GPU'))
+
+            device = "/device:GPU:0" if num_gpus_available > 0 else "/CPU:0"
+            self._log.debug(f"Training on device : {device}")
+
+            self._distribution_strategy = tf.distribute.OneDeviceStrategy(device=device)
+
+    def _create_tf_dataset(self, multiple_choice_samples):
+        choice_collator = MultipleChoiceCollator(
+            pad_token_idx=self._hf_tokenizer.pad_token_id,
+            max_sequence_length=self._opt_max_sequence_length)
+
+        sample_generator = CollatedSampleGenerator(multiple_choice_samples, choice_collator)
+
+        return tf.data.Dataset\
+            .from_generator(
+                sample_generator,
+                output_types=(tf.int64, tf.int64, tf.int64, tf.int64, tf.int8))\
+            .shuffle(20, reshuffle_each_iteration=True) \
+            .batch(self._global_batch_size)
+
+    def _setup_batch_datasets(self):
+        self._batch_training_set = self._create_tf_dataset(self._training_set)
+        self._batch_validation_set = self._create_tf_dataset(self._validation_set)
+
+        # TODO: revisit the behavior of MLPUG when the total number of batches is not known before hand.
+        num_samples = len(self._training_set)
+        self._num_batches_per_epoch = int(num_samples/self._global_batch_size)
+        if num_samples % self._global_batch_size != 0:
+            self._num_batches_per_epoch += 1
+
+        if self.is_distributed:
+            strategy = self._distribution_strategy
+
+            # Distribute training and validation set
+            self._batch_training_set = strategy.experimental_distribute_dataset(self._batch_training_set)
+            self._batch_validation_set = strategy.experimental_distribute_dataset(self._batch_validation_set)
+
+    def _build_model(self):
+        model_config = self._gather_model_config()
+
+        self._log.info(f"Loading pre-trained GPT-2 model : {self._args.pretrained_model}")
+
+        # Building pre-trained GPT-2 model
+        with self._distribution_strategy.scope():
+            self._model = TFGPT2DoubleHeadsModel(model_config)
+            self._model.resize_token_embeddings(new_num_tokens=self._orig_num_tokens + self._num_special_tokens)
+
+            if self._args.activation_checkpointing:
+                self._model.gradient_checkpointing_enable()
+
+        self._log.info(f"Configuration of loaded model : \n{model_config}")
+
+    def _setup_training_model(self):
+        with self._distribution_strategy.scope():
+            self._training_model = TrainModel(
+                self._model,
+                self._args.lm_loss_weight,
+                self._global_batch_size)
+
+    def _build_optimizer(self):
+        with self._distribution_strategy.scope():
+            self._optimizer = AdamWeightDecay(
+                learning_rate=self._args.learning_rate,
+                weight_decay_rate=self._args.weight_decay,
+                beta_1=0.9,
+                beta_2=0.999,
+                epsilon=1e-08,
+                exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
+
+    def _get_custom_trainer_config(self):
+        """
+        Implementation depends on specific ML Library you are using
+
+        :return:
+        """
+
+        # Tensorflow only needs batch_data_signature as additional argument, compared to PyTorch
+        return {
+            "batch_data_signature": self._batch_training_set.element_spec
+        }
+
+    def _create_gather_classification_data_function(self):
+        return gather_next_sentence_prediction_data
+
+    def _create_gather_distributed_classification_data_function(self):
+        # Not required by Tensorflow
+        return None
+
+    def _create_clean_up_batch_data_func(self):
+        return clean_up_batch_data
+
+    def _get_custom_evaluator_config(self):
+        """
+        Implementation depends on specific ML Library you are using
+
+        :return:
+        """
+
+        # Tensorflow only needs distribution_strategy as additional argument, compared to PyTorch
+        return {
+            "distribution_strategy": self._distribution_strategy
+        }
+
+    def _add_lr_scheduler_callback(self):
+        """
+        Returns function that can cleanup the batch data to optimize memory use
+
+        Implementation depends on specific ML Library you are using
+
+        :return:
+        """
+        def update_lr(iter, lr):
+            return lr * self._lr_scheduling_func(iter)
+
+        self._callbacks += [mlp.callbacks.LRSchedulerWrapper({
+                'warmup-scheduler': tf.keras.callbacks.LearningRateScheduler(update_lr)
+            }, batch_level=True)]
+
+
+if __name__ == '__main__':
+    # ############# SETUP LOGGING #############
+    mlp.logging.use_fancy_colors()
+    logger = get_logger(os.path.basename(__file__))
+    # ########################################
+
+    # ############## PARSE ARGS ##############
+    parser = create_arg_parser()
+
+    parser.parse_args()
+
+    args = parser.parse_args()
+
+    describe_args(args, logger)
+
+    # ############## TRAIN MODEL ##############
+    if args.distributed:
+        num_gpus_available = len(tf.config.list_physical_devices('GPU'))
+
+        if num_gpus_available < 1:
+            logger.error(f"No GPUs available for data distributed training over multiple GPUs")
+            exit(-1)
+
+        num_devices = args.num_devices if args.num_devices > 0 else num_gpus_available
+        if num_devices > num_gpus_available:
+            logger.warn(f"Number of requested GPUs is lower than available GPUs, "
+                        f"limiting training to {num_gpus_available} GPUS")
+            num_devices = num_gpus_available
+
+        logger.info(f"Distributing batch training over {num_devices} GPUs.")
+
+        global_batch_size = args.batch_size * num_devices
+    else:
+        num_devices = 1
+        logger.info(f"Single device mode.")
+        global_batch_size = args.batch_size
+
+    logger.info(f"Global batch size: {args.batch_size}")
+
+    training_process = TrainingProcess(args, num_devices=num_devices)
+    training_process.setup()
+    training_process.start()
