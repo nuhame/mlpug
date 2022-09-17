15a9fd1286df8c9e0f50a501ef0f6786f10249c7
Freddy Snijder <freddy@visionscapers.com>
2022-09-17 14:49:27 +0200

    Made activation checkpointing flag available for both Tensorflow and Pytorch (but not sure if available for Tensorflow).

--- examples/chatbot/pytorch/shared_args.py
+++ examples/chatbot/pytorch/shared_args.py
@@ -10,11 +10,6 @@ def create_arg_parser(description="Finetune GPT2 as persona aware chatbot using 
         type=int, required=False, default=2,
         help='Number of dataloader workers.')
 
-    parser.add_argument(
-        '--activation-checkpointing',
-        action='store_true',
-        help='Enable activation checkpointing')
-
     return parser
 
 
@@ -22,5 +17,3 @@ def describe_args(args, logger):
     logger.info(f"Num. dataloader workers: {args.num_dataloader_workers}")
 
     describe_base_args(args, logger)
-
-    logger.info(f"Activation checkpointing: {args.activation_checkpointing}")
