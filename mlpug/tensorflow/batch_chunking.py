f93b8c1dd94ce86d946b48b96e9b6cb9c33ff440
Freddy Snijder <freddy@visionscapers.com>
2023-09-20 22:50:20 +0200

    Intermediate commit. Trying to remove unnecessary strategy.run calls in DistributedChunkableBatchDataset (they create warnings because they are not wrapped in tf.function, which is on purpose)

--- mlpug/tensorflow/batch_chunking.py
+++ mlpug/tensorflow/batch_chunking.py
@@ -1,28 +1,22 @@
 from functools import cached_property
-
-import tensorflow as tf
-from tensorflow.types.experimental.distributed import PerReplica
+from typing import List
 
 from mlpug.base import Base
-from mlpug.batch_chunking import convert_to_chunkable_dataset, get_total_batch_size, get_num_chunks, ChunkableBatchWrapper
+from mlpug.batch_chunking import (
+    convert_to_chunkable_dataset,
+    get_total_batch_size,
+    get_num_chunks,
+    ChunkableBatchWrapper
+)
 
-
-def get_first_replica_result(func, distribution_strategy, per_replica_input):
-    per_replica_results = distribution_strategy.run(
-        func,
-        args=(per_replica_input,)
-    )
-
-    per_replica_results = distribution_strategy.experimental_local_results(per_replica_results)
-
-    return per_replica_results[0]
+from tensorflow.python.distribute import values
 
 
 class DistributedChunkableBatchDataset(Base):
 
     def __init__(
             self,
-            batch,
+            per_replica_batch,
             chunkable_batch_wrapper: ChunkableBatchWrapper,
             batch_chunk_size: int,
             # TODO: unable to import Strategy from tensorflow.distribute
@@ -36,10 +30,15 @@ class DistributedChunkableBatchDataset(Base):
         """
         super().__init__()
 
-        self._per_replica_dataset = distribution_strategy.run(
-            convert_to_chunkable_dataset,
-            args=(batch, chunkable_batch_wrapper, batch_chunk_size)
-        )
+        per_replica_batch = distribution_strategy.experimental_local_results(per_replica_batch)
+        first_batch_replica = per_replica_batch[0]
+
+        if not isinstance(first_batch_replica, (list, tuple)):
+            raise ValueError(f"The per replica batch must be a list or tuple of tensors. "
+                             f"Type of first batch replica is: {type(first_batch_replica)}")
+
+        self._per_replica_dataset = [convert_to_chunkable_dataset(b, chunkable_batch_wrapper, batch_chunk_size)
+                                     for b in per_replica_batch]
 
         self._distribution_strategy = distribution_strategy
 
@@ -49,27 +48,17 @@ class DistributedChunkableBatchDataset(Base):
 
     @cached_property
     def total_batch_size(self):
-        # All replicas will have the same total_batch_size
-        return get_first_replica_result(
-            get_total_batch_size,
-            self._distribution_strategy,
-            self._per_replica_dataset
-        )
+        total_batch_size = [get_total_batch_size(dataset) for dataset in self._per_replica_dataset]
+        return total_batch_size[0]
 
     def __len__(self):
-        return get_first_replica_result(
-            get_num_chunks,
-            self._distribution_strategy,
-            self._per_replica_dataset
-        )
+        num_chunks = [get_num_chunks(dataset) for dataset in self._per_replica_dataset]
+        return num_chunks[0]
 
     def __iter__(self):
         self._num_chunks = len(self)
         self._chunk_idx = -1
-        self._per_replica_iter = self._distribution_strategy.run(
-            iter,
-            args=(self._per_replica_dataset,)
-        )
+        self._per_replica_iter = [iter(dataset) for dataset in self._per_replica_dataset]
 
         return self
 
@@ -78,7 +67,6 @@ class DistributedChunkableBatchDataset(Base):
         if self._chunk_idx >= self._num_chunks:
             raise StopIteration()
 
-        return self._distribution_strategy.run(
-            next,
-            args=(self._per_replica_iter,)
-        )
+        # IMPORTANT This assumes that the batch is a tuple or list of tensors
+        per_replica_batch = tuple(values.PerReplica(t) for t in zip(*[next(it) for it in self._per_replica_iter]))
+        return per_replica_batch
