5967892ce46864f12f04e176ceebc41e105e330e
Freddy Snijder <freddy@visionscapers.com>
2023-10-26 20:19:11 +0200

    Limit what is imported to the MLPug space

--- mlpug/pytorch/trainers/callbacks/__init__.py
+++ mlpug/pytorch/trainers/callbacks/__init__.py
@@ -1,8 +1,10 @@
-from .basic import *
-from .callback import *
+from .basic import LogProgress, BatchSizeLogger, DescribeLogsObject
+from .callback import Callback
 from .checkpoint_manager import CheckpointManager
 from .lr_scheduler_wrapper import LRSchedulerWrapper
-from .metrics_logger import MetricsLoggingMode, TrainingMetricsLogger, DatasetMetricsLogger
+from .metrics_logger import TrainingMetricsLogger, DatasetMetricsLogger
 from .tensorboard import Tensorboard, AutoTensorboard
 from .distributed import DistributedSamplerManager
 from .cuda_memory import EmptyCudaCache, LogCudaMemory
+
+from mlpug.trainers.callbacks.metrics_logger import MetricsLoggingMode
