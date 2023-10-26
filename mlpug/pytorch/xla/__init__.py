563e21b0711b74a0e96e4e03c5e65ac11b0f99c1
Freddy Snijder <freddy@visionscapers.com>
2023-10-26 20:23:42 +0200

    Updated Pytorch/XLA MLPug code based on latest changes of the framework

--- mlpug/pytorch/xla/__init__.py
+++ mlpug/pytorch/xla/__init__.py
@@ -1,13 +1,18 @@
 from . import trainers
 from .trainers import callbacks
 
+# TODO only export limited set of relevant classes and functions
 from . import evaluation
+
+from . import batch_chunking
+
+from . import scheduler_funcs
 
 import mlpug.mlpug_logging as logging
 from mlpug.base import Base
 
 from mlpug.multi_processing import MultiProcessingManager
-from mlpug.pytorch.xla.multi_processing import XLADistributedContext
+from mlpug.pytorch.xla.multi_processing import MultiProcessingMixin, XLADistributedContext
 
 if not isinstance(MultiProcessingManager.get_context(), XLADistributedContext):
     MultiProcessingManager.set_context(XLADistributedContext())
