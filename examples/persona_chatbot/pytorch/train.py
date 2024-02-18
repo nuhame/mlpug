Mix of topics: Added --use-mixed-precision, made --lm-loss-weight default 1.0, fixed small commenting issues.

examples/persona_chatbot/pytorch/train.py
@@ -122,7 +122,7 @@ class TrainModel(torch.nn.Module):
         }
 
 
-# See TrainingProcessBase for more information on the different methods implemented here
+# See TrainingProcessBase for more information on the different methods implemented here.
 # Here we implement the methods that are specific to our problem and specific to our ML library, PyTorch.
 class TrainingProcess(TrainingProcessBase):
 